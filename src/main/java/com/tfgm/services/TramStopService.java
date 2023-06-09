64fbdff4119ade938dd1e035f921a20da68cf4f6
AelinaXY <Lauren@2infinity.net>
2023-06-09 13:17:21 +0100

    Refactor TramStopService's constructor

--- src/main/java/com/tfgm/services/TramStopService.java
+++ src/main/java/com/tfgm/services/TramStopService.java
@@ -7,7 +7,6 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.stream.Collectors;
 import org.apache.http.HttpEntity;
@@ -27,50 +26,62 @@ public class TramStopService {
   private final TramStopServiceUtilities utilities = new TramStopServiceUtilities();
 
   public TramStopService(String tramDataPath) throws IOException {
+
+    // Reads all of the TramStopData from the static JSON file.
     JSONArray allTramStopData =
         new JSONArray(
             Files.readAllLines(Paths.get(tramDataPath)).stream()
                 .map(String::valueOf)
                 .collect(Collectors.joining()));
 
-    // System.out.println(allTramStopData);
-
+    // Iterates through and adds all the tram stops to a hashmap
     for (int i = 0; i < allTramStopData.length(); i++) {
-      JSONObject tempStop = allTramStopData.getJSONObject(i);
+      JSONObject currentTramStop = allTramStopData.getJSONObject(i);
+      String tramStopName = currentTramStop.getString("tramStopName");
+
       tramStopHashMap.put(
-          tempStop.getString("tramStopName"),
+          tramStopName,
           new TramStop(
-              tempStop.getString("location"),
-              tempStop.getString("direction"),
-              tempStop.getString("line")));
+              // Gets Tram Stop name as shown in official material
+              currentTramStop.getString("location"),
+              // Gets direction
+              currentTramStop.getString("direction"),
+              // Gets line
+              currentTramStop.getString("line")));
     }
+
+    // Iterates through the tram stop array again to create connections between stations. This is so
+    // that we can be sure that all the stops exist before making connections
     for (int i = 0; i < allTramStopData.length(); i++) {
-      JSONObject tempStop = allTramStopData.getJSONObject(i);
 
-      TramStop currentTramStop = tramStopHashMap.get(tempStop.getString("tramStopName"));
+      JSONObject currentTramJson = allTramStopData.getJSONObject(i);
+      String tramStopName = currentTramJson.getString("tramStopName");
 
-      currentTramStop.setPrevAndNextStops(
-          tempStop.getJSONArray("prevStop").toList().stream()
-              .map(tramStopHashMap::get)
-              .map(TramStopContainer::new)
-              .toArray(TramStopContainer[]::new),
-          tempStop.getJSONArray("nextStop").toList().stream()
-              .map(tramStopHashMap::get)
-              .map(TramStopContainer::new)
-              .toArray(TramStopContainer[]::new));
+      TramStop currentTramStop = tramStopHashMap.get(tramStopName);
+
+      JSONArray previousStopJsonArray = currentTramJson.getJSONArray("prevStop");
+      JSONArray nextStopJsonArray = currentTramJson.getJSONArray("nextStop");
+
+      TramStopContainer[] previousStops =
+          utilities.findStopLinks(previousStopJsonArray, tramStopHashMap);
+      TramStopContainer[] nextStops = utilities.findStopLinks(nextStopJsonArray, tramStopHashMap);
+
+      currentTramStop.setPrevAndNextStops(previousStops, nextStops);
     }
 
+    // Iterates through the hashmap and ensures that there is a consistent link stop between two
+    // nodes in the graph
     for (TramStop tramStop : tramStopHashMap.values()) {
 
       System.out.println(tramStop.toString());
-      for (TramStopContainer n : tramStop.getNextStops()) {
-        TramStopContainer nextStopPrevReference =
-            Arrays.stream(n.getTramStop().getPrevStops())
-                .filter(s -> s.getTramStop().equals(tramStop))
-                .toList()
-                .get(0);
 
-        nextStopPrevReference.setTramLinkStop(n.getTramLinkStop());
+      for (TramStopContainer n : tramStop.getNextStops()) {
+        TramStopContainer nextStopsContainerToThisStop =
+            utilities.findTramStopContainerToTramStop(n, tramStop);
+
+        // Makes sure that this tram stops link stop to the next tram stop is equal to the next tram
+        // stops link stop to this tram stop
+        nextStopsContainerToThisStop.setTramLinkStop(n.getTramLinkStop());
       }
     }
 
@@ -94,10 +105,15 @@ public class TramStopService {
     HttpClient httpclient = HttpClients.createDefault();
 
     URIBuilder builder = new URIBuilder("https://api.tfgm.com/odata/Metrolinks");
-    // "StationLocation":"Withington","AtcoCode":"9400ZZMAWIT2","Direction":"Outgoing","Dest0":"East
-    // Didsbury","Carriages0":"Single","Status0":"Due","Wait0":"3","Dest1":"East
-    // Didsbury","Carriages1":"Double","Status1":"Due","Wait1":"14","Dest2":"East
-    // Didsbury","Carriages2":"Single","Status2":"Due","Wait2":"16","Dest3":"","Carriages3":"","Status3":"","MessageBoard":"Welcome to Metrolink. For up to date travel information contact Metrolink twitter on @MCRMetrolink or visit www.TfGM.com. Take care of each other Manchester.","Wait3":"","LastUpdated":"2023-05-26T15:29:19Z"
+    // Example data from TFGM API
+    // {"StationLocation":"Withington", "AtcoCode":"9400ZZMAWIT2", "Direction":"Outgoing",
+    // "Dest0":"East Didsbury", "Carriages0":"Single", "Status0":"Due", "Wait0":"3",
+    // "Dest1":"East Didsbury", "Carriages1":"Double", "Status1":"Due", "Wait1":"14",
+    // "Dest2":"East Didsbury", "Carriages2":"Single", "Status2":"Due", "Wait2":"16",
+    // "Dest3":"", "Carriages3":"", "Status3":"", "Wait3":"",
+    // "MessageBoard":"Welcome to Metrolink. For up to date travel information contact Metrolink
+    // twitter on @MCRMetrolink or visit www.TfGM.com. Take care of each other Manchester.",
+    // "LastUpdated":"2023-05-26T15:29:19Z" }
 
     URI uri = builder.build();
     HttpGet request = new HttpGet(uri);
@@ -118,10 +134,10 @@ public class TramStopService {
           String nextDestination = currentStation.getString("Dest" + j);
           boolean nextDestinationNull = nextDestination.equals("");
 
-          if (!(nextDestinationNull)) {
+          if (!nextDestinationNull) {
             String nextStatus = currentStation.getString("Status" + j);
-            boolean isTramDeparting = currentStation.equals("Departing");
-            boolean isTramArriving = currentStation.equals("Arrived");
+            boolean isTramDeparting = nextStatus.equals("Departing");
+            boolean isTramArriving = nextStatus.equals("Arrived");
 
             if (isTramDeparting || isTramArriving) {
               String rawStationName = currentStation.getString("StationLocation");
--- src/main/java/com/tfgm/services/TramStopServiceUtilities.java
+++ src/main/java/com/tfgm/services/TramStopServiceUtilities.java
@@ -1,5 +1,10 @@
 package com.tfgm.services;
 
+import com.tfgm.models.TramStop;
+import com.tfgm.models.TramStopContainer;
+import java.util.Arrays;
+import java.util.HashMap;
+import org.json.JSONArray;
 import org.json.JSONObject;
 
 public class TramStopServiceUtilities {
@@ -14,5 +19,25 @@ public class TramStopServiceUtilities {
 
   public String cleanStationName(String stationName) {
     return stationName.replaceAll("[^A-Za-z]+", "");
+  }
+
+  public TramStopContainer[] findStopLinks(
+      JSONArray arrayOfStops, HashMap<String, TramStop> tramStopHashMap) {
+    // Takes a JSON array of stops and finds them in the hashmap. The found stops are converted to
+    // TramStopContainers and added to an array. This is so that queues may be added to the edges of
+    // the graph
+    return arrayOfStops.toList().stream()
+        .map(tramStopHashMap::get)
+        .map(TramStopContainer::new)
+        .toArray(TramStopContainer[]::new);
+  }
+
+  public TramStopContainer findTramStopContainerToTramStop(
+      TramStopContainer nextStopLink, TramStop originalTramStop) {
+    // Finds the container that references the passed in tram stop
+    return Arrays.stream(nextStopLink.getTramStop().getPrevStops())
+        .filter(s -> s.getTramStop().equals(originalTramStop))
+        .toList()
+        .get(0);
   }
 }
