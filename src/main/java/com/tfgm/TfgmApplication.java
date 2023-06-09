1281c04d61e390c6d0be5c7663ef59990e08dc59
AelinaXY <Lauren@2infinity.net>
2023-06-09 13:33:06 +0100

    Add application ready event

--- src/main/java/com/tfgm/TfgmApplication.java
+++ src/main/java/com/tfgm/TfgmApplication.java
@@ -1,19 +1,34 @@
 package com.tfgm;
 
-import com.tfgm.controllers.TramController;
+import com.tfgm.services.TramStopService;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.util.concurrent.TimeUnit;
-
-import com.tfgm.services.TramStopService;
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 
 @SpringBootApplication
 public class TfgmApplication {
 
   public static void main(String[] args)
       throws URISyntaxException, IOException, InterruptedException {
-    //    SpringApplication.run(TfgmApplication.class, args);
+    SpringApplication.run(TfgmApplication.class, args);
+    //    int count = 0;
+    //    TramStopService tramService =
+    //        new TramStopService("src/main/resources/static/TramStopData.json");
+    //
+    //    while (true) {
+    //      System.out.println("---------LOOOOP---------" + count);
+    //      tramService.update();
+    //      TimeUnit.SECONDS.sleep(10);
+    //      count += 10;
+    //    }
+  }
+
+  @EventListener(ApplicationReadyEvent.class)
+  public void mainRunner() throws IOException, URISyntaxException, InterruptedException {
     int count = 0;
     TramStopService tramService =
         new TramStopService("src/main/resources/static/TramStopData.json");
